4eb6a9894129c51e9126aeb4b92a7463436f0120
gonzalezjuampi-hub <gonzalez.juampi@gmail.com>
2026-06-04 00:29:54 -0300

    Enhance service worker notification handling

--- sw.js
+++ sw.js
@@ -1,4 +1,33 @@
-self.addEventListener('notificationclick', function(event) {
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+// Este es el motor que recibe la orden de tu index.html y despierta a Android
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SHOW_NOTIF') {
+    const options = {
+      body: event.data.body,
+      vibrate: [200, 100, 200],
+      requireInteraction: true,
+      tag: 'med-alarm-pwa'
+    };
+    self.registration.showNotification(event.data.title, options);
+  }
+});
+
+self.addEventListener('notificationclick', (event) => {
   event.notification.close();
-  event.waitUntil(clients.openWindow('/'));
+  event.waitUntil(
+    clients.matchAll({ type: 'window' }).then((clientList) => {
+      if (clientList.length > 0) {
+        return clientList[0].focus();
+      }
+      return clients.openWindow('/');
+    })
+  );
 });
